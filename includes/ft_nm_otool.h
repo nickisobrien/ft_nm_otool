6c17973ec2819f4e9213d712e3dfbf37849ce87a
Nicholas O'brien <nobrien@e1z3r1p23.42.us.org>
2018-10-08 15:37:30 -0700

    basic nm

--- includes/ft_nm_otool.h
+++ includes/ft_nm_otool.h
@@ -2,5 +2,11 @@
 #define __FT_NM_OTOOL_H
 
 # include "../libft/includes/libft.h"
+# include <sys/mman.h>
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <stdlib.h>
 
 #endif
